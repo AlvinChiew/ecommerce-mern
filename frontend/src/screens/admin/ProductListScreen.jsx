6ebee04058d86fd0cec46bcd7305a56079097e83
alvinchiew <jingjie95@gmail.com>
2024-01-15 00:51:56 +0800

    add product pagination

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,8 +1,10 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import {
   useGetProductsQuery,
   useDeleteProductMutation,
@@ -11,14 +13,18 @@ import {
 import { toast } from 'react-toastify';
 
 const ProductListScreen = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error, refetch } = useGetProductsQuery({
+    pageNumber,
+  });
 
   const [deleteProduct, { isLoading: loadingDelete }] =
     useDeleteProductMutation();
 
   const deleteHandler = async (id) => {
     if (window.confirm('Are you sure')) {
-      try {
+      try { 
         await deleteProduct(id);
         refetch();
       } catch (err) {
@@ -43,12 +49,12 @@ const ProductListScreen = () => {
 
   return (
     <>
-      <Row className="align-items-center">
+      <Row className='align-items-center'>
         <Col>
           <h1>Products</h1>
         </Col>
-        <Col className="text-end">
-          <Button className="my-3" onClick={createProductHandler}>
+        <Col className='text-end'>
+          <Button className='my-3' onClick={createProductHandler}>
             <FaPlus /> Create Product
           </Button>
         </Col>
@@ -59,10 +65,10 @@ const ProductListScreen = () => {
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{error}</Message>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <>
-          <Table striped bordered hover responsive className="table-sm">
+          <Table striped bordered hover responsive className='table-sm'>
             <thead>
               <tr>
                 <th>ID</th>
@@ -74,7 +80,7 @@ const ProductListScreen = () => {
               </tr>
             </thead>
             <tbody>
-              {products.map((product) => (
+              {data.products.map((product) => (
                 <tr key={product._id}>
                   <td>{product._id}</td>
                   <td>{product.name}</td>
@@ -83,13 +89,13 @@ const ProductListScreen = () => {
                   <td>{product.brand}</td>
                   <td>
                     <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                      <Button variant="light" className="btn-sm mx-2">
+                      <Button variant='light' className='btn-sm mx-2'>
                         <FaEdit />
                       </Button>
                     </LinkContainer>
                     <Button
-                      variant="danger"
-                      className="btn-sm"
+                      variant='danger'
+                      className='btn-sm'
                       onClick={() => deleteHandler(product._id)}
                     >
                       <FaTrash style={{ color: 'white' }} />
@@ -99,7 +105,7 @@ const ProductListScreen = () => {
               ))}
             </tbody>
           </Table>
-          {/* PAGINATE PLACEHOLDER */}
+          <Paginate pages={data.pages} page={data.page} isAdmin={true} />
         </>
       )}
     </>

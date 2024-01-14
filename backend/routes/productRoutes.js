7c33e88ae57539af5ede1f2fed6b069c1d9c8660
alvinchiew <jingjie95@gmail.com>
2024-01-15 00:11:03 +0800

    add product review

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
@@ -16,5 +17,6 @@ productRoutes
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+productRoutes.route('/:id/reviews').post(protect, createProductReview);
 
 export default productRoutes;
